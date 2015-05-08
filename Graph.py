aca8f0c0adcbbb3c768282dc59b2780473510243
Patch67 <patrickabiggs@gmail.com>
2015-05-08 11:51:27 +0100

    Added to to_gcode method to Graph classes
    added abstract to_gcode to Graph.
    Implemented to_gcode in Line and Rectangle.  Other classes just got a
    Gcode comment.

--- Graph.py
+++ Graph.py
@@ -4,8 +4,6 @@ Patrick Biggs
 20th April 2015
 """
 
-'''Graphics Classes'''
-
 import abc
 
 
@@ -18,7 +16,11 @@ class Graph:
         pass
     
     @abc.abstractmethod
-    def save(self):
+    def save(self, file):
+        pass
+
+    @abc.abstractmethod
+    def to_gcode(self, file):
         pass
 
 
@@ -33,6 +35,10 @@ class Line(Graph):
     def save(self, file):
         file.write("LINE %d, %d, %d, %d\n" % (self.x0, self.y0, self.x1, self.y1))
 
+    def to_gcode(self, file):
+        file.write("G0 X%d Yd%\n" % (self.x0, self.y0))  # Move
+        file.write("G1 X%d Yd%\n" % (self.x1, self.y1))  # End
+
 
 class Circle(Graph):
     def __init__(self, x, y, r):
@@ -42,6 +48,9 @@ class Circle(Graph):
 
     def save(self, file):
         file.write("CIRCLE %d, %d, %d\n" % (self.x, self.y, self.r))
+
+    def to_gcode(self, file):
+        file.write("(Circle %d, %d radius %d" % (self.x, self.y, self.r))
 
 
 class Rectangle(Graph):
@@ -54,6 +63,13 @@ class Rectangle(Graph):
     def save(self, file):
         file.write("RECTANGLE %d, %d, %d, %d\n" % (self.x0, self.y0, self.x1, self.y1))
 
+    def to_gcode(self, file):
+        file.write("G0 X%d Yd%\n" % (self.x0, self.y0))  # Move
+        file.write("G1 X%d Yd%\n" % (self.x1, self.y0))  # Right
+        file.write("G1 X%d Yd%\n" % (self.x1, self.y1))  # Up
+        file.write("G1 X%d Yd%\n" % (self.x0, self.y1))  # Left
+        file.write("G1 X%d Yd%\n" % (self.x0, self.y0))  # Down
+
 
 class Text(Graph):
     def __init__(self, x, y, text):
@@ -63,6 +79,9 @@ class Text(Graph):
 
     def save(self, file):
         file.write("TEXT %d, %d, %s\n" % (self.x, self.y, self.text1))
+
+    def to_gcode(self, file):
+        file.write("(Text is %s)\n" % self.text)
 
 
 # Implements the composite pattern
@@ -79,3 +98,7 @@ class Group(Graph):
         for child in self.children:
             child.save(file)
 
+    def to_gcode(self, file):
+        file.write("( Group: %s )\n" % self.name)
+        for child in self.children:
+            child.to_gcode(file)
